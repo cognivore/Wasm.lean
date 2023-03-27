6f5a459aad47f211958c298ee2e9ba30d3798f53
cognivore <66186054+cognivore@users.noreply.github.com>
2023-03-27 22:17:25 +0100

    Problem: Unsure if block runs like ref. impl.
    Solution:
    
     - Port many tasks from ref. impl. test suite. Make sure that they only use supported feature set.
     - Bring supported feature tracking checkbox from Github into /SUPPORTED.md, and gitignore it.

--- Tests/RuntimeCompatibility.lean
+++ Tests/RuntimeCompatibility.lean
@@ -22,7 +22,8 @@ https://github.com/yatima-inc/Wasm.lean/pull/53
 partial def testGeneric : String → Int → Int → TestSeq
   | mod, os, rs =>
     let str := s!"Running function _main_ : () -> I32 evaluates the same in Lean and Rust.
-    {mod}"
+    {mod}
+    ! If failed, remember: first is ours, second is rust) !"
     test str $ os = rs
 
 open IO.FS in
@@ -46,7 +47,129 @@ def basics :=
      )"
   ]
 
+def supported := [
+
+  "(module ;;deep
+        (func (export \"main\") (result i32)
+          (block (result i32) (block (result i32)
+            (block (result i32) (block (result i32)
+              (block (result i32) (block (result i32)
+                (block (result i32) (block (result i32)
+                  (block (result i32) (block (result i32)
+                    (block (result i32) (block (result i32)
+                      (block (result i32) (block (result i32)
+                        (block (result i32) (block (result i32)
+                          (block (result i32) (block (result i32)
+                            (block (result i32) (block (result i32)
+                              (block (result i32) (block (result i32)
+                                (block (result i32) (block (result i32)
+                                  (block (result i32) (block (result i32)
+                                    (block (result i32) (block (result i32)
+                                      (block (result i32) (block (result i32)
+                                        (block (result i32) (block (result i32)
+                                          (block (result i32) (block (result i32)
+                                            (block (result i32) (block (result i32)
+                                              (block (result i32) (block (result i32)
+                                                ;; (nop) (i32.const 150)
+                                                (i32.const 150)
+                                              ))
+                                            ))
+                                          ))
+                                        ))
+                                      ))
+                                    ))
+                                  ))
+                                ))
+                              ))
+                            ))
+                          ))
+                        ))
+                      ))
+                    ))
+                  ))
+                ))
+              ))
+            ))
+          ))
+    )
+)",
+
+"(module (func (export \"main\") (result i32)
+  (block (nop))
+  (block (result i32) (i32.const 7))
+))",
+
+"(module (func (export \"main\") (result i32)
+  (block (nop) (nop) (nop) (nop))
+  (block (result i32)
+    (nop) (nop) (nop) (i32.const 7) (nop)
+  )
+  (drop)
+  (block (result i32 i64 i32)
+    (nop) (nop) (nop) (i32.const 8) (nop)
+    (nop) (nop) (nop) (i64.const 7) (nop)
+    (nop) (nop) (nop) (i32.const 9) (nop)
+  )
+  (drop) (drop)
+))",
+
+"(module (func (export \"main\") (result i32)
+  (block (result i32)
+    (block (nop) (block) (nop))
+    (block (result i32) (nop) (i32.const 9))
+  )
+))",
+
+"(module (func (export \"main\") (result i32) ;; empty
+  (block)
+  (block $l)
+  (i32.const 0)
+))",
+
+"(module (func (export \"main\") (result i32)
+  (select (block (result i32) (i32.const 1)) (i32.const 2) (i32.const 3))
+))",
+"(module (func (export \"main\") (result i32)
+  (select (i32.const 2) (block (result i32) (i32.const 1)) (i32.const 3))
+))",
+"(module (func (export \"main\") (result i32)
+  (select (i32.const 2) (i32.const 3) (block (result i32) (i32.const 1)))
+))",
+
+"(module (func (export \"main\") (result i32)
+  (loop (result i32) (block (result i32) (i32.const 1)) (nop) (nop))
+))",
+"(module (func (export \"main\") (result i32)
+  (loop (result i32) (nop) (block (result i32) (i32.const 1)) (nop))
+))",
+"(module (func (export \"main\") (result i32)
+  (loop (result i32) (nop) (nop) (block (result i32) (i32.const 1)))
+))",
+
+"(module (func (export \"main\") (result i32)
+  (if (result i32) (i32.const 1) (then (block (result i32) (i32.const 1))) (else (i32.const 2)))
+))",
+"(module (func (export \"main\") (result i32)
+  (if (result i32) (i32.const 1) (then (i32.const 2)) (else (block (result i32) (i32.const 1))))
+))",
+
+"(module (func (export \"main\") (result i32)
+  (block (result i32) (br_if 0 (block (result i32) (i32.const 1)) (i32.const 2)))
+))",
+"(module (func (export \"main\") (result i32)
+  (block (result i32) (br_if 0 (i32.const 2) (block (result i32) (i32.const 1))))
+))",
+
+"(module (func (export \"main\") (result i32)
+  (block (result i32) (block (result i32) (i32.const 1)) (i32.const 2) (br_table 0 0))
+))",
+"(module (func (export \"main\") (result i32)
+  (block (result i32) (i32.const 2) (block (result i32) (i32.const 1)) (br_table 0 0))
+))"
+
+]
+
 def main : IO UInt32 := do
   match (← doesWasmSandboxRun?) with
-  | .ok _ => withWasmSandboxRun runWasmTestSeq [ basics ]
+  | .ok _ => withWasmSandboxRun runWasmTestSeq [ basics, supported ]
   | _ => withWasmSandboxFail
