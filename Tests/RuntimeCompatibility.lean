3884429c09bf5bcdb74046cf5557a57a3d340cd1
cognivore <66186054+cognivore@users.noreply.github.com>
2023-03-28 15:26:59 +0100

    Problem: It's unclear if branching works as in ref. impl.
    Solution:
    
     - Select those tests that should be fully supported by Wasm.lean
     - Paste them into RuntimeCompatibility
     - Run standard import macro on them:
    
    ```
    0i"(module <Esc>
    /"<CR>i\<Esc>
    /"i<CR>\<Esc>
    0/(<CR>n%a)",<Esc>
    j
    ```

--- Tests/RuntimeCompatibility.lean
+++ Tests/RuntimeCompatibility.lean
@@ -165,8 +165,213 @@ def supported := [
 ))",
 "(module (func (export \"main\") (result i32)
   (block (result i32) (i32.const 2) (block (result i32) (i32.const 1)) (br_table 0 0))
-))"
+))",
 
+"(module (func (export \"main\") (block (drop (i32.ctz (br 0))))))",
+"(module (func (export \"main\") (block (drop (i64.ctz (br 0))))))",
+"(module (func (export \"main\") (block (drop (f32.neg (br 0))))))",
+"(module (func (export \"main\") (block (drop (f64.neg (br 0))))))",
+"(module (func (export \"main\") (block (drop (i32.add (br 0))))))",
+"(module (func (export \"main\") (block (drop (i64.add (br 0))))))",
+"(module (func (export \"main\") (block (drop (f32.add (br 0))))))",
+"(module (func (export \"main\") (block (drop (f64.add (br 0))))))",
+"(module (func (export \"main\") (result i32)
+  (block (result i32) (i32.ctz (br 0 (i32.const 1))))
+))",
+"(module (func (export \"main\") (result i64)
+  (block (result i64) (i64.ctz (br 0 (i64.const 2))))
+))",
+"(module (func (export \"main\") (result f32)
+  (block (result f32) (f32.neg (br 0 (f32.const 3))))
+))",
+"(module (func (export \"main\") (result f64)
+  (block (result f64) (f64.neg (br 0 (f64.const 4))))
+))",
+"(module (func (export \"main\") (result f64 f64)
+  (block (result f64 f64)
+    (f64.add (br 0 (f64.const 4) (f64.const 5))) (f64.const 6)
+  )
+))",
+
+"(module (func (export \"main\")
+  (block (br 0) (nop))
+))",
+"(module (func (export \"main\")
+  (block (nop) (br 0) (nop))
+))",
+"(module (func (export \"main\")
+  (block (nop) (nop) (br 0))
+))",
+"(module (func (export \"main\") (result i32)
+  (block (result i32) (nop) (nop) (br 0 (i32.const 2)))
+))",
+
+"(module (func (export \"main\") (result i32)
+  (block (result i32) (loop (result i32) (br 1 (i32.const 3)) (i32.const 2)))
+))",
+"(module (func (export \"main\") (result i32)
+  (block (result i32)
+    (loop (result i32) (nop) (br 1 (i32.const 4)) (i32.const 2))
+  )
+))",
+"(module (func (export \"main\") (result i32)
+  (block (result i32)
+    (loop (result i32) (nop) (nop) (br 1 (i32.const 5)))
+  )
+))",
+
+"(module (func (export \"main\") (result i32)
+  (block (result i32) (br 0 (br 0 (i32.const 9))))
+))",
+
+"(module (func (export \"main\")
+  (block (br_if 0 (br 0)))
+))",
+"(module (func (export \"main\") (result i32)
+  (block (result i32)
+    (drop (br_if 0 (br 0 (i32.const 8)) (i32.const 1))) (i32.const 7)
+  )
+))",
+"(module (func (export \"main\") (result i32)
+  (block (result i32)
+    (drop (br_if 0 (i32.const 6) (br 0 (i32.const 9)))) (i32.const 7)
+  )
+))",
+
+"(module (func (export \"main\")
+  (block (br_table 0 0 0 (br 0)))
+))",
+"(module (func (export \"main\") (result i32)
+  (block (result i32)
+    (br_table 0 0 0 (br 0 (i32.const 10)) (i32.const 1)) (i32.const 7)
+  )
+))",
+"(module (func (export \"main\") (result i32)
+  (block (result i32)
+    (br_table 0 0 (i32.const 6) (br 0 (i32.const 11))) (i32.const 7)
+  )
+))",
+
+"(module (func (export \"main\") (result i32)
+  (block (result i32)
+    (if (result i32) (br 0 (i32.const 2))
+      (then (i32.const 0))
+      (else (i32.const 1))
+    )
+  )
+))",
+"(module (func (export \"main\") (param i32 i32) (result i32)
+  (block (result i32)
+    (if (result i32) (local.get 0)
+      (then (br 1 (i32.const 3)))
+      (else (local.get 1))
+    )
+  )
+))",
+"(module (func (export \"main\") (param i32 i32) (result i32)
+  (block (result i32)
+    (if (result i32) (local.get 0)
+      (then (local.get 1))
+      (else (br 1 (i32.const 4)))
+    )
+  )
+))",
+"(module (func (export \"main\") (param i32 i32) (result i32)
+  (block (result i32)
+    (select (br 0 (i32.const 5)) (local.get 0) (local.get 1))
+  )
+))",
+"(module (func (export \"main\") (param i32 i32) (result i32)
+  (block (result i32)
+    (select (local.get 0) (br 0 (i32.const 6)) (local.get 1))
+  )
+))",
+"(module (func (export \"main\") (result i32)
+  (block (result i32)
+    (select (i32.const 0) (i32.const 1) (br 0 (i32.const 7)))
+  )
+))",
+"(module (func (export \"main\") (result i32)
+  (block (result i32) (select (br 0 (i32.const 8))))
+))",
+"(module (func (export \"main\") (result i32)
+  (i32.add
+    (i32.const 1)
+    (block (result i32)
+      (nop)
+      (i32.add (i32.const 4) (br 0 (i32.const 8)))
+    )
+  )
+))",
+"(module (func (export \"main\") (result i32)
+  (i32.add
+    (i32.const 1)
+    (block (result i32)
+      (drop (i32.const 2))
+      (drop
+        (block (result i32)
+          (drop (i32.const 4))
+          (br 0 (br 1 (i32.const 8)))
+        )
+      )
+      (i32.const 16)
+    )
+  )
+))",
+
+"(module (func (export \"main\") (result i32)
+  (i32.add
+    (i32.const 1)
+    (block (result i32)
+      (drop (i32.const 2))
+      (drop
+        (block (result i32)
+          (drop (i32.const 4))
+          (drop (br_if 0 (br 1 (i32.const 8)) (i32.const 1)))
+          (i32.const 32)
+        )
+      )
+      (i32.const 16)
+    )
+  )
+))",
+
+"(module (func (export \"main\") (result i32)
+  (i32.add
+    (i32.const 1)
+    (block (result i32)
+      (drop (i32.const 2))
+      (drop (br_if 0 (i32.const 4) (br 0 (i32.const 8))))
+      (i32.const 16)
+    )
+  )
+))",
+"(module  (func (export \"main\") (result i32)
+   (i32.add
+     (i32.const 1)
+     (block (result i32)
+       (drop (i32.const 2))
+       (drop
+         (block (result i32)
+           (drop (i32.const 4))
+           (br_table 0 (br 1 (i32.const 8)) (i32.const 1))
+         )
+       )
+       (i32.const 16)
+     )
+   )
+ ))",
+
+"(module  (func (export \"main\") (result i32)
+   (i32.add
+     (i32.const 1)
+     (block (result i32)
+       (drop (i32.const 2))
+       (br_table 0 (i32.const 4) (br 0 (i32.const 8)))
+       (i32.const 16)
+     )
+   )
+ ))"
 ]
 
 def main : IO UInt32 := do
